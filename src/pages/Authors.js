2c18be26611d5a6e23e53144128f20570c3d3836
Vmart1234 <70496070+Vmart1234@users.noreply.github.com>
2024-02-18 22:34:54 -0500

    Update Authors.js

--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -10,7 +10,7 @@ export default function Authors() {
                 <ul>
                     <li><Link to="/vmart" >Victor Martinez</Link></li>
                     <li><Link to="/hossain">Istiaque Hossain </Link></li>
-                    <li><Link to="/amran">Amran</Link></li>
+                    <li><Link to="/amran">Amran Rahim </Link></li>
                     <li><Link to="/sabbir"> Sabbir Ahmed</Link> </li>
                 </ul>
             
